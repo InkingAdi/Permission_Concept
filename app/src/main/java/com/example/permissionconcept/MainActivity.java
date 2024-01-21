fbbf17370524c7d227313e3de38d4ae464d5906e
ADITYA SHARMA <aadditayaa1010@gmail.com>
2024-01-21 23:42:22 +0530

    Added different permission in main activity

--- app/src/main/java/com/example/permissionconcept/MainActivity.java
+++ app/src/main/java/com/example/permissionconcept/MainActivity.java
@@ -13,8 +13,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //initialize permission value
-    Integer CAMERA_PERMISSION = 100;    //We are using this variable for comparison whether permission is given or not.
-    Integer SMS_PERMISSION = 101;
+    Integer CAMERA_PERMISSION_REQ = 100;    //We are using this variable for comparison whether permission is given or not.
+    Integer SMS_R_REQ = 101;
+    Integer CONTACT_PERMISSION_REQ = 102;
+    Integer STORAGE_R_REQ = 103;
+    Integer STORAGE_W_REQ = 104;
+    Integer FINE_LOCATION_REQ = 105;
+    Integer SMS_S_REQ = 106;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,46 +33,55 @@ public class MainActivity extends AppCompatActivity {
          */
 
         findViewById(R.id.btn_camera).setOnClickListener(view -> {
-            checkCameraPermission(CAMERA_PERMISSION);
+            checkCameraPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_REQ);
         });
 
         findViewById(R.id.btn_contact).setOnClickListener(view -> {
+            //Read Contact
+            checkCameraPermission(Manifest.permission.READ_CONTACTS, CONTACT_PERMISSION_REQ);
 
         });
 
         findViewById(R.id.btn_read_storage).setOnClickListener(view -> {
-
+            checkCameraPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_R_REQ);
         });
 
         findViewById(R.id.btn_write_storage).setOnClickListener(view -> {
-
+            if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_W_REQ);
+            }
         });
 
         findViewById(R.id.btn_location).setOnClickListener(view -> {
-
+            if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQ);
+            }
         });
 
         findViewById(R.id.btn_read_sms).setOnClickListener(view -> {
-
+            if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_SMS) == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_SMS}, SMS_R_REQ);
+            }
         });
 
         findViewById(R.id.btn_send_sms).setOnClickListener(view -> {
-
+            if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SEND_SMS},SMS_S_REQ);
+            }
         });
     }
 
-    private void checkCameraPermission(Integer request_code) {
+    private void checkCameraPermission(String permi, Integer request_code) {
 
         //Check If Permission is granted or not
-        int perm = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
-
         //IF PERMISSION IS DENIED
-        if(perm == PackageManager.PERMISSION_DENIED){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, permi) == PackageManager.PERMISSION_DENIED){
             //This will display dialog box to user for granting permission
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},request_code);
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{permi},request_code);
         }else {
             Toast.makeText(this,"User Permission Granted !",Toast.LENGTH_LONG).show();
         }
+
     }
 
     //This function will be called when user accept or decline the permission
@@ -77,11 +91,64 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == CAMERA_PERMISSION){
+        if(requestCode == CAMERA_PERMISSION_REQ){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this,"Camera Permission Granted !", Toast.LENGTH_LONG).show();
             }else {
                 Toast.makeText(this,"Camera Permission Denied !", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        //If Requested permission is contact permission
+        if(requestCode == CONTACT_PERMISSION_REQ){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Read permission Granted for Contacts",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Read permission for Contacts Not Granted !",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        //If Requested permission is STORAGE READ permission
+        if(requestCode == STORAGE_R_REQ){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Read permission Granted for STORAGE",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Read permission for STORAGE Not Granted !",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if(requestCode == STORAGE_W_REQ){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "WRITE permission Granted for STORAGE",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"WRITE permission for STORAGE Not Granted !",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        //If Requested permission is FINE LOCATION
+        if(requestCode == FINE_LOCATION_REQ){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Location permission Granted !", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Location permission Denied !", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        //READ SMS PERMISSION
+        if(requestCode == SMS_R_REQ){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "READ SMS permission Granted !", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "READ SMS permission Denied !", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        //SEND SMS PERMISSION
+        if(requestCode == SMS_S_REQ){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "SEND SMS permission Granted !", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "SEND SMS permission Denied !", Toast.LENGTH_SHORT).show();
             }
         }
     }
